98cdfa870882ab4e3a01458593cfbc095b509261
robiyyaa <97520492+robiyyaa@users.noreply.github.com>
2022-10-03 19:21:57 +0600

    Delete src directory

--- src/Cat.java
+++
@@ -1,6 +0,0 @@
-public class Cat implements Animal {
-    @Override
-    public String makeSound() {
-        return "Meow Meow";
-    }
-}
--- src/Dog.java
+++
@@ -1,6 +0,0 @@
-public class Dog implements Animal {
-    @Override
-    public String makeSound() {
-        return "Bark Bark";
-    }
-}
